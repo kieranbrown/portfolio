031ea5c74c7f80af4e8dca866dbdada488cf5f58
Kieran Brown <kieran.brown@chatloop.com>
2025-01-19 22:29:13 +0000

    ci: configure astro & biome checks with pre-commit (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,32 @@
-import { defineConfig, envField } from "astro/config";
-import cloudflare from '@astrojs/cloudflare';
+import { parseArgs } from "node:util";
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-import { parseArgs } from 'node:util';
+import { defineConfig, envField } from "astro/config";
 import { loadEnv } from "vite";
 
-const { values: { mode } } = parseArgs({
-  allowPositionals: true,
+const {
+  values: { mode },
+} = parseArgs({
+  options: { mode: { type: "string", default: process.env.NODE_ENV } },
   strict: false,
-  options: {
-    mode: { type: "string", default: process.env.NODE_ENV }
-  },
 });
 
-const { SITE_URL } = loadEnv(mode, process.cwd(), '');
+const { SITE_URL } = loadEnv(mode, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   env: {
     schema: {
-      SITE_URL: envField.string({ context: "client", access: "public", optional: true }),
-    }
+      SITE_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: true,
+        url: true,
+      }),
+    },
   },
   integrations: [tailwind()],
-  output: 'server',
-  site: SITE_URL ?? 'http://localhost:4321',
+  output: "server",
+  site: SITE_URL ?? "http://localhost:4321",
 });
--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -1,23 +1,26 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
 
 const site = import.meta.env.SITE;
 
-export async function GET() {
+export async function GET(context: APIContext) {
   const posts = await getCollection("posts");
 
   return rss({
-    title: 'KB Blog',
-    description: 'Yet another DevOps Engineer\'s blog',
+    title: "KB Blog",
+    description: "Yet another DevOps Engineer's blog",
     site: site,
     trailingSlash: false,
-    xmlns: { atom: 'http://www.w3.org/2005/Atom' },
-    customData: `<atom:link href="${site}/posts/feed.xml" rel="self" type="application/rss+xml"/>`,
-    items: posts.toSorted((a, b) => b.data.date.getTime() - a.data.date.getTime()).map((post) => ({
-      title: post.data.title,
-      description: post.data.description,
-      pubDate: post.data.date,
-      link: `/posts/${post.slug}`,
-    })),
+    xmlns: { atom: "http://www.w3.org/2005/Atom" },
+    customData: `<atom:link href="${site + context.url.pathname}" rel="self" type="application/rss+xml"/>`,
+    items: posts
+      .toSorted((a, b) => b.data.date.getTime() - a.data.date.getTime())
+      .map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.date,
+        link: `/posts/${post.slug}`,
+      })),
   });
 }
