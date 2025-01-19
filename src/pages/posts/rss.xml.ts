562c9875af9711e34fede7c032343b0862d94851
Kieran Brown <kieran.brown@chatloop.com>
2025-01-19 21:46:03 +0000

    ci: fix build errors

--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -1,13 +1,13 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function GET() {
   const posts = await getCollection("posts");
 
   return rss({
     title: 'KB Blog',
     description: 'Yet another DevOps Engineer\'s blog',
-    site: context.site,
+    site: import.meta.env.SITE,
     trailingSlash: false,
     items: posts.toSorted((a, b) => b.data.date.getTime() - a.data.date.getTime()).map((post) => ({
       title: post.data.title,
