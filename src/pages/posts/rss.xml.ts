c175e8982be710e9b1cf29519441cfb460bf45b4
Kieran Brown <kieran.brown@chatloop.com>
2025-01-19 22:14:04 +0000

    fix: add rss feed compatibility with feedly (#3)

--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -1,14 +1,18 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
+const site = import.meta.env.SITE;
+
 export async function GET() {
   const posts = await getCollection("posts");
 
   return rss({
     title: 'KB Blog',
     description: 'Yet another DevOps Engineer\'s blog',
-    site: import.meta.env.SITE,
+    site: site,
     trailingSlash: false,
+    xmlns: { atom: 'http://www.w3.org/2005/Atom' },
+    customData: `<atom:link href="${site}/posts/feed.xml" rel="self" type="application/rss+xml"/>`,
     items: posts.toSorted((a, b) => b.data.date.getTime() - a.data.date.getTime()).map((post) => ({
       title: post.data.title,
       description: post.data.description,
